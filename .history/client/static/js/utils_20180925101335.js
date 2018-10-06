9139c2f960db8f116cab34ebcac7e3d8636638eb
cty <1250680250@qq.com>
2018-10-06 13:56:11 +0800

    后台管理模块vuex

---
+++ .history/client/static/js/utils_20180925101335.js
@@ -0,0 +1,24 @@
+function add0(m) {
+    return m < 10 ? '0' + m : m;
+}
+
+export function timeStamp() {
+    const time = new Date();
+    const y = time.getFullYear();
+    const m = time.getMonth() + 1;
+    const d = time.getDate();
+    const h = time.getHours();
+    const mm = time.getMinutes();
+    const s = time.getSeconds();
+    const ms = time.getMilliseconds();
+    return "" + y + add0(m) + add0(d) + add0(h) + add0(mm) + add0(s) + add0(ms);
+}
+
+module.exports = (name) => {
+    let SEARCH = window.location.search;
+    let reg = new RegExp(`(\\?|&)${name}(\\[\\])?=([^&]*)`);
+    let value = SEARCH.match(reg);
+  
+    return value ? value[3] : '';
+  };
+  
---
+++ .history/server/api/routes/articles_20180925103323.js
@@ -0,0 +1,13 @@
+import * as $ from '../../controllers/articleController.js';
+import verify from '../../middleware/verify.js';
+
+export default async(router) => {
+    router
+        .get('/articles', $.getAllArticles)
+        .get('/publishArticles', $.getAllPublishedArticle)
+        .get(`/article?id=`, $.getArticleById)
+        .post('/createArticles', $.createArticle)
+        .post('/uploadArticleImage', $.uploadArticleImage)
+        .patch('/articles/:id', $.updateArticle)
+        .delete('/articles/:id', $.deleteArticle);
+};
